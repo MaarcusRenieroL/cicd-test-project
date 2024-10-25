178f350b2e7b53a637027e037b4a32a6b7f9ac8e
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-10-25 10:21:45 +0530

    feat: add test controller

---
+++ src/main/java/com/maarcus/cicd_test/controller/TestApi.java
@@ -0,0 +1,13 @@
+package com.maarcus.cicd_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestApi {
+  
+  @GetMapping("/")
+  public String hello() {
+    return "Suprise MOTHERFAKAAR!!!!";
+  }
+}
